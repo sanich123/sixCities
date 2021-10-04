476682760990c90f460ed3d3270f541a8b5a84d9
Alexander <70276651+sanich123@users.noreply.github.com>
2021-10-04 17:53:56 +0300

    Update project/src/components/favorites/favorites.tsx
    Co-authored-by: Artem <artrudov@gmail.com>

--- project/src/components/favorites/favorites.tsx
+++ project/src/components/favorites/favorites.tsx
@@ -162,7 +162,13 @@ function Favorites(): JSX.Element {
         </main>
         <footer className="footer container">
           <a className="footer__logo-link" href="main.html">
-            <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
+            <img 
+              className="footer__logo" 
+              src="img/logo.svg" 
+              alt="6 cities logo" 
+              width="64" 
+              height="33"
+            />
           </a>
         </footer>
       </div>
