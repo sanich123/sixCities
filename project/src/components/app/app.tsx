41bd548cd581e9eca9313aba0a316af8a00a9a97
sanich123 <aovoronin.piano@gmail.com>
2021-10-05 14:49:41 +0300

    Added different path to different pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,8 @@
 import MainScreen from '../main/main';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { AppRoute } from '../../components/const ';
+import LogIn from '../login/login';
+import Favorites from '../favorites/favorites';
 
 type Cards = number[];
 
@@ -8,7 +12,25 @@ type AppProps = {
 }
 
 function App({placesCount, cards}: AppProps): JSX.Element {
-  return <MainScreen placesCount={placesCount} cards={cards} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainScreen
+            placesCount={placesCount} cards={cards}
+          />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <LogIn />
+        </Route>
+        <Route exact path={AppRoute.Favorites}>
+          <Favorites />
+        </Route>
+        <Route exact path={AppRoute.Room} />
+      </Switch>
+    </BrowserRouter>
+  );
+  // return <MainScreen placesCount={placesCount} cards={cards} />;
 }
 
 export default App;
---
+++ project/src/components/const .ts
@@ -0,0 +1,6 @@
+export enum AppRoute {
+  Main = '/',
+  SignIn = '/login',
+  Favorites = '/favorites',
+  Room = '/offer/:id'
+}
