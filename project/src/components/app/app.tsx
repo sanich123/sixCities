9881739a78977e63ea21b15ae60b34c6b427f071
sanich123 <aovoronin.piano@gmail.com>
2021-10-08 19:00:54 +0300

    Сделал через map генерацию страничек

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,66 +1,32 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import LogIn from '../login/login';
-import Properties from '../properties/properties';
 import PrivateRoute from '../private-route';
 import Favorites from '../favorites/favorites';
-import MainScreen from '../main/main';
-import Page404 from '../page404/page404';
-import { AppRoute, AuthorizationStatus } from '../../components/const ';
+import { AuthorizationStatus } from '../../components/const ';
 
 type AppProps = {
-  placesCount: number,
-  cards: number[]
+  pages: {
+    component: any,
+    isPrivate: boolean,
+    route: string,
+  }[]
 }
 
-function App({placesCount, cards}: AppProps): JSX.Element {
+function App({pages}: AppProps): JSX.Element {
+  const simplyRoute = pages.map(({component, route, isPrivate}) =>
+    isPrivate === false ?
+      <Route exact path={route}>{component}</Route>
+      :
+      <PrivateRoute exact path={route} authorizationStatus={AuthorizationStatus.NO_AUTH} render={() => <Favorites />} />,
+  );
+
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={AppRoute.Main}>
-          <MainScreen
-            placesCount={placesCount} cards={cards}
-          />
-        </Route>
-        <Route exact path={AppRoute.SignIn}>
-          <LogIn />
-        </Route>
-        <PrivateRoute
-          exact
-          path={AppRoute.Favorites}
-          authorizationStatus={AuthorizationStatus.AUTH}
-          render={() => <Favorites />}
-        >
-        </PrivateRoute>
-        <Route exact path={AppRoute.Room}>
-          <Properties />
-        </Route>
-        <Route>
-          <Page404 />
-        </Route>
+        {simplyRoute}
       </Switch>
     </BrowserRouter>
   );
 }
-// const array = [{
-//   component: 'MainScreen',
-//   isPrivate: false,
-//   route: AppRoute.Main,
-// },{
-//   component: 'LogIn',
-//   isPrivate: false,
-//   route: AppRoute.SignIn,
-// },{
-//   component: 'Favorites',
-//   isPrivate: true,
-//   route: AppRoute.Favorites,
-// }, {
-//   component: 'Properties',
-//   isPrivate: false,
-//   route: AppRoute.Room,
-// }];
 
-// array.map((it) => <Route exact path={it.route}>
-//   <{it.component} />
-//         </Route>);
 
 export default App;
