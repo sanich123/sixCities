92cadc6eff5893abae75e4c2206ce5668e1ac4a7
sanich123 <aovoronin.piano@gmail.com>
2021-10-13 12:13:16 +0300

    Some refactoring

--- project/src/components/properties/properties.tsx
+++ project/src/components/properties/properties.tsx
@@ -11,8 +11,7 @@ type propertiesProps = {
 }
 
 function Properties({reviews, offers}: propertiesProps): JSX.Element {
-  const history = useHistory();
-  const uniqUrl = +history.location.pathname.split('').slice(8).join('');
+  const uniqUrl = +useHistory().location.pathname.split('').slice(8).join('');
 
   const [uniqOffer] = offers.slice().filter(({id}) => id === uniqUrl);
   const nearPlaces = offers.slice().filter(({city, id}) => city.name === uniqOffer.city.name && id !== uniqOffer.id);
@@ -129,45 +128,46 @@ function Properties({reviews, offers}: propertiesProps): JSX.Element {
             <section className="property__map map"></section>
           </section>
           <div className="container">
-            <section className="near-places places">
-              <h2 className="near-places__title">Other places in the neighbourhood</h2>
-              <div className="near-places__list places__list">
-                {nearPlaces.map((place) => nearPlaces ? (
-                  <article key={place.id} className="near-places__card place-card">
-                    {place.isPremium ? <div className="place-card__mark"><span>Premium</span></div> : ''}
-                    <div className="near-places__image-wrapper place-card__image-wrapper">
-                      <Link to={`/offer/:${place.id}`}>
-                        <img className="place-card__image" src={place.previewImage} width="260" height="200" alt="" />
-                      </Link>
-                    </div>
-                    <div className="place-card__info">
-                      <div className="place-card__price-wrapper">
-                        <div className="place-card__price">
-                          <b className="place-card__price-value">&euro;{place.price}</b>
-                          <span className="place-card__price-text">&#47;&nbsp;night</span>
-                        </div>
-                        <button className={place.isFavorite ? 'place-card__bookmark-button button place-card__bookmark-button--active' : 'place-card__bookmark-button button'} type="button">
-                          <svg className="place-card__bookmark-icon" width="18" height="19">
-                            <use xlinkHref="#icon-bookmark"></use>
-                          </svg>
-                          <span className="visually-hidden">{`${place.isFavorite ? 'In' : 'To'}bookmarks`}</span>
-                        </button>
+            {nearPlaces.length ?
+              <section className="near-places places">
+                <h2 className="near-places__title">Other places in the neighbourhood</h2>
+                <div className="near-places__list places__list">
+                  { nearPlaces.map((place) => (
+                    <article key={ place.id } className="near-places__card place-card">
+                      { place.isPremium ? <div className="place-card__mark"><span>Premium</span></div> : ''}
+                      <div className="near-places__image-wrapper place-card__image-wrapper">
+                        <Link to={`/offer/:${place.id}`}>
+                          <img className="place-card__image" src={place.previewImage} width="260" height="200" alt="" />
+                        </Link>
                       </div>
-                      <div className="place-card__rating rating">
-                        <div className="place-card__stars rating__stars">
-                          <span style={{ width: `${place.rating / 5 * 100}%` }}></span>
-                          <span className="visually-hidden">Rating</span>
+                      <div className="place-card__info">
+                        <div className="place-card__price-wrapper">
+                          <div className="place-card__price">
+                            <b className="place-card__price-value">&euro;{ place.price }</b>
+                            <span className="place-card__price-text">&#47;&nbsp;night</span>
+                          </div>
+                          <button className={place.isFavorite ? 'place-card__bookmark-button button place-card__bookmark-button--active' : 'place-card__bookmark-button button'} type="button">
+                            <svg className="place-card__bookmark-icon" width="18" height="19">
+                              <use xlinkHref="#icon-bookmark"></use>
+                            </svg>
+                            <span className="visually-hidden">{`${place.isFavorite ? 'In' : 'To'}bookmarks`}</span>
+                          </button>
                         </div>
+                        <div className="place-card__rating rating">
+                          <div className="place-card__stars rating__stars">
+                            <span style={{ width: `${place.rating / 5 * 100}%` }}></span>
+                            <span className="visually-hidden">Rating</span>
+                          </div>
+                        </div>
+                        <h2 className="place-card__name">
+                          <Link to={`/offer/:${ place.id }`}>{ place.title }</Link>
+                        </h2>
+                        <p className="place-card__type">{ place.type }</p>
                       </div>
-                      <h2 className="place-card__name">
-                        <Link to={`/offer/:${place.id}`}>{place.title}</Link>
-                      </h2>
-                      <p className="place-card__type">{place.type}</p>
-                    </div>
-                  </article>
-                ) : '')}
-              </div>
-            </section>
+                    </article>
+                  ))}
+                </div>
+              </section> : ''}
           </div>
         </main>
       </div>
