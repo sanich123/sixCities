204bf332ca16d20a4ff889b7a73b1085195aa40c
sanich123 <aovoronin.piano@gmail.com>
2021-10-15 12:14:08 +0300

    Added right key, again

--- project/src/components/properties/review-form/review-form.tsx
+++ project/src/components/properties/review-form/review-form.tsx
@@ -15,7 +15,7 @@ function ReviewForm(): JSX.Element {
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
         {Object.entries(Marks).reverse().map(([key, value]) => (
-          <>
+          <div key={value}>
             <input
               className="form__rating-input visually-hidden"
               key={value}
@@ -38,7 +38,7 @@ function ReviewForm(): JSX.Element {
                 <use xlinkHref="#icon-star"></use>
               </svg>
             </label>
-          </>
+          </div>
         ))}
       </div>
 
