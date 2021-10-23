40158f4ed77be05dc1d95714161832558d6a39f9
sanich123 <aovoronin.piano@gmail.com>
2021-10-23 15:15:17 +0300

    Added right favorites, when empty offers

--- project/src/components/favorites/favorites.tsx
+++ project/src/components/favorites/favorites.tsx
@@ -16,19 +16,28 @@ function Favorites({ offers }: favoriteProps): JSX.Element {
     <>
       <Sprite />
 
-      <div className="page">
+      <div className={ `${!favoriteOffers.length && 'page--favorites-empty'} page`}>
 
         <Header />
 
-        <main className="page__main page__main--favorites">
+        <main className={ `${!favoriteOffers.length && 'page__main--favorites-empty'} page__main page__main--favorites`}>
           <div className="page__favorites-container container">
-            <section className="favorites">
-              <h1 className="favorites__title">Saved listing</h1>
-              <ul className="favorites__list">
-                { uniqueСities.map((city) =>
-                  (<FavoritesList city={ city } offers={ favoriteOffers } key={ city } />)) }
-              </ul>
-            </section>
+            { favoriteOffers.length ?
+              <section className="favorites">
+                <h1 className="favorites__title">Saved listing</h1>
+                <ul className="favorites__list">
+                  { uniqueСities.map((city) =>
+                    (<FavoritesList city={ city } offers={ favoriteOffers } key={ city } />)) }
+                </ul>
+              </section>
+              :
+              <section className="favorites favorites--empty">
+                <h1 className="visually-hidden">Favorites (empty)</h1>
+                <div className="favorites__status-wrapper">
+                  <b className="favorites__status">Nothing yet saved.</b>
+                  <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+                </div>
+              </section>}
           </div>
         </main>
 
