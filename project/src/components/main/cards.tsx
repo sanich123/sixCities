c7609fe09f85de6825c18946c48db41276a7473b
Alexander <70276651+sanich123@users.noreply.github.com>
2021-10-24 19:38:24 +0300

    Update project/src/components/main/cards.tsx
    Co-authored-by: Artem <artrudov@gmail.com>

--- project/src/components/main/cards.tsx
+++ project/src/components/main/cards.tsx
@@ -13,7 +13,15 @@ type OnHover = {
 function Cards( { offers, onHover }: cardsProps & OnHover): JSX.Element {
   return (
     <div className="cities__places-list places__list tabs__content">
-      { offers.map(({ id, ...rest }) => <Card id={ id } key={ id } onHover={ onHover } { ...rest } modificator={ MAIN } />) }
+      { offers.map(({ id, ...rest }) => (
+        <Card 
+          id={ id } 
+          key={ id } 
+          onHover={ onHover } 
+          { ...rest } 
+          modificator={ MAIN } 
+        />
+        ))}
     </div>
   );
 }
