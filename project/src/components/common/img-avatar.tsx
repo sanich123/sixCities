649f532c2848d95cc55a0b1494a54947300db33b
Alexander <70276651+sanich123@users.noreply.github.com>
2021-10-24 19:16:27 +0300

    Update project/src/components/common/img-avatar.tsx
    Co-authored-by: Artem <artrudov@gmail.com>

--- project/src/components/common/img-avatar.tsx
+++ project/src/components/common/img-avatar.tsx
@@ -6,11 +6,11 @@ type ImageProps = {
 function Image({ avatarUrl, uniqUrl}: ImageProps): JSX.Element {
   return (
     <img
-      className={`${uniqUrl ? 'property' : 'reviews'}__avatar user__avatar`}
+      className={`${uniqUrl ? 'property' : 'reviews'}__avatar user__avatar`} // почему не используешь библиотеку classnames?
       src={ avatarUrl }
-      width={`${ uniqUrl ? '74' : '54'}` }
-      height={`${ uniqUrl ? '74' : '54'}` }
-      alt={`${ uniqUrl ? 'Host ' : 'Reviews'}avatar` }
+      width={uniqUrl ? '74' : '54'}
+      height={uniqUrl ? '74' : '54' }
+      alt={uniqUrl ? 'Host ' : 'Reviews'}avatar}
     />
   );
 }
