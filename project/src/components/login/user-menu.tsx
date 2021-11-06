2b24c599f38e8cf663bdd92b55de4f8d1ac66df5
sanich123 <aovoronin.piano@gmail.com>
2021-11-06 14:43:26 +0300

    Изменил userMenu

--- project/src/components/login/user-menu.tsx
+++ project/src/components/login/user-menu.tsx
@@ -1,19 +1,42 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+
+
 function UserMenu(): JSX.Element {
+  const authStatus = useSelector(({ authorizationStatus }) => authorizationStatus);
+  const authEmail = useSelector(({ authorizationEmail }) => authorizationEmail);
   return (
     <nav className="header__nav">
       <ul className="header__nav-list">
-        <li className="header__nav-item user">
-          <a className="header__nav-link header__nav-link--profile" href="/">
-            <div className="header__avatar-wrapper user__avatar-wrapper">
-            </div>
-            <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-          </a>
-        </li>
-        <li className="header__nav-item">
-          <a className="header__nav-link" href="/">
-            <span className="header__signout">Sign out</span>
-          </a>
-        </li>
+        {authStatus === AuthorizationStatus.AUTH ? (
+          <>
+            <li className="header__nav-item user">
+              <Link className="header__nav-link header__nav-link--profile" to="/">
+                <div className="header__avatar-wrapper user__avatar-wrapper">
+                </div>
+                <span className="header__user-name user__name">{authEmail}</span>
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link
+                className="header__nav-link"
+                to={AppRoute.Main}
+              >
+                <span className="header__signout">Sign out</span>
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li className="header__nav-item user">
+            <Link className="header__nav-link header__nav-link--profile" to={AppRoute.SignIn}>
+              <div className="header__avatar-wrapper user__avatar-wrapper">
+              </div>
+              <span className="header__login">Sign in</span>
+            </Link>
+          </li>
+        )}
+
       </ul>
     </nav>
   );
