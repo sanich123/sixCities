e31655f2c62a5a852ec079e9736d05b13409bff2
sanich123 <aovoronin.piano@gmail.com>
2021-11-09 18:12:46 +0300

    Добавил валидацию пароля

--- project/src/components/login/login.tsx
+++ project/src/components/login/login.tsx
@@ -1,29 +1,36 @@
 import Sprite from '../common/sprite';
 import Logo from '../common/logo';
-import { FormEvent, useRef } from 'react';
+import { FormEvent, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginAction } from '../../store/api-actions';
 import { AppRoutes } from '../../const';
 import { useHistory } from 'react-router';
 import { State } from '../../types/reducer';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const WRONG_PASSWORD = 'Пароль должен состоять минимум из одной буквы и одной цифры';
 
 function LogIn(): JSX.Element {
   const history = useHistory();
   const dispatch = useDispatch();
   const currentCity = useSelector(({ city }: State) => city);
-  const loginRef = useRef<HTMLInputElement | null>(null);
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
   const passwordRef = useRef<HTMLInputElement | null>(null);
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-
-    if (loginRef.current !== null && passwordRef.current !== null) {
+    const testPassword = /([A-zА-я]{1}[0-9]{1})|([0-9]{1}[A-zА-я]{1})/;
+    if (testPassword.test(password)) {
       dispatch(loginAction({
-        login: loginRef.current.value,
-        password: passwordRef.current.value,
+        login: email,
+        password: password,
       }));
       history.push(AppRoutes.Main);
+    }
+    else {
+      toast.warn(WRONG_PASSWORD);
     }
   };
 
@@ -58,7 +65,7 @@ function LogIn(): JSX.Element {
                 <div className="login__input-wrapper form__input-wrapper">
                   <label className="visually-hidden">E-mail</label>
                   <input
-                    ref={ loginRef }
+                    onChange={ ({ target }) => setEmail(target.value)}
                     className="login__input form__input"
                     type="email"
                     name="email"
@@ -69,12 +76,13 @@ function LogIn(): JSX.Element {
                 <div className="login__input-wrapper form__input-wrapper">
                   <label className="visually-hidden">Password</label>
                   <input
-                    ref={ passwordRef }
                     className="login__input form__input"
                     type="password"
                     name="password"
                     placeholder="Password"
                     required
+                    onChange={ ({ target }) => setPassword(target.value) }
+                    ref={passwordRef}
                   />
                 </div>
                 <button className="login__submit form__submit button" type="submit">Sign in</button>
